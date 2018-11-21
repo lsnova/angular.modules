ede85ce473e86af67da0b3d7301cef586b573bf7
Ryszard Bałut <ryszardb@lsnova.pl>
2018-11-21 14:28:50 +0100

    modified exports

--- lib/lsn-libs.d.ts
+++ lib/lsn-libs.d.ts
@@ -2,3 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
+export { NumericDirective as ɵa } from './lib/directives/numeric/numeric.directive';
--- lib/public_api.d.ts
+++ lib/public_api.d.ts
@@ -1,2 +1,1 @@
 export * from './lib/lsn-libs.module';
-export * from './lib/directives/numeric/numeric.directive';
