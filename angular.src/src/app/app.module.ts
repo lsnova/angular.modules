740dff01b91d5f92fc982615be44c3749e42e775
Ryszard Bałut <ryszardb@lsnova.pl>
2018-11-22 14:57:49 +0100

    angular.src project config

--- angular.src/src/app/app.module.ts
+++ angular.src/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {NumericDirective} from 'lsn-modules';
+import {FormsModule} from '@angular/forms';
+
+import {NumericDirective} from '@lsnova/angularmodules';
 
 import {AppComponent} from './app.component';
 
