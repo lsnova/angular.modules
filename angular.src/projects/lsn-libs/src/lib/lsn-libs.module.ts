d89cb19bf9012bcfdbf4f41a9b162fef1592ebdb
Ryszard Bałut <ryszardb@lsnova.pl>
2019-10-07 10:31:49 +0200

    import change

--- angular.src/projects/lsn-libs/src/lib/lsn-libs.module.ts
+++ angular.src/projects/lsn-libs/src/lib/lsn-libs.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {LsnNumericModule} from './directives/numeric/numeric.module';
 import {LsnNumpadModule} from './directives/numpad/numpad.module';
 import {LsnLatinToGreekModule} from './directives/latin-to-greek/latin-to-greek.module';
 import {LsnCapitalizeModule} from './directives/capitalize/capitalize.module';
 
 import {LsnMatSelectModule} from './components/mat-select/mat-select.module';
-import {FormsModule} from '@angular/forms';
-import { LsnScrollSpyModule } from './directives/scroll-spy';
+import {LsnScrollSpyModule} from './directives/scroll-spy/scroll-spy.module';
 
 @NgModule({
   declarations: [],
