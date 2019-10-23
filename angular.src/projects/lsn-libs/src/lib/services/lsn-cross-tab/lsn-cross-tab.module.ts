135b9d33bad5d828fad0c71274a84a58ce7837d9
Zieliński Jakub <jakubz@lsnova.pl>
2019-10-23 14:26:30 +0200

    cross modules import witch module name

--- angular.src/projects/lsn-libs/src/lib/services/lsn-cross-tab/lsn-cross-tab.module.ts
+++ angular.src/projects/lsn-libs/src/lib/services/lsn-cross-tab/lsn-cross-tab.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LsnCrossTabService} from './lsn-cross-tab.service';
-import {LsnCookieService} from '../lsn-cookie/lsn-cookie.service';
+import {LsnCookieService} from '@lsnova/angularmodules/services/lsn-cookie';
 
 @NgModule({
-    providers: [LsnCrossTabService, LsnCookieService],
-    imports: [
-        CommonModule
-    ]
+  providers: [LsnCrossTabService, LsnCookieService],
+  imports: [
+    CommonModule
+  ]
 })
 export class LsnCrossTabModule {
 }
